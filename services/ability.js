1d1486684c01dab3d086bec8128f2e4770a1c8db
Fannypc <mars.poc@gmail.com>
2022-11-22 15:28:13 -0600

    dev: removing comments

--- services/ability.js
+++ services/ability.js
@@ -1,5 +1,3 @@
-// const { Pokemon, Pokemon_Abilities, Ability, Type } = require("../models");
-
 const { Pool } = require("pg");
 const pool = new Pool({
   user: "postgres",
@@ -15,18 +13,7 @@ const readAbilities = async (req, res) => {
     // console.log(err, result);
     res.send(result);
   });
-  // res.sendStatus(200);
 };
-
-// const createAbility = async (request, response, next) => {
-//   try {
-//     const ability = await Ability.create(request.body);
-//     response.json(ability);
-//   } catch (err) {
-//     console.log(err);
-//     next(err);
-//   }
-// };
 
 function close() {
   return pool.end();
@@ -35,5 +22,4 @@ function close() {
 module.exports = {
   readAbilities,
   close,
-  //   createAbility,
 };
